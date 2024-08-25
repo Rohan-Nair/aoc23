8c9b721604bb43afe45abbdef4527ddb90b20455
rohan <rohannair018@gmail.com>
2024-08-25 18:07:27 +0530

    read the contents of the file

--- done/src/main.rs
+++ done/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs; 
 fn main() {
+    // read the file from the file system 
+    let contents = fs::read_to_string("./src/assets/done1.txt")
+        .expect("Something went wrong reading the file");
+
+
+    println!("With text:\n{contents}");
+
     
 }
